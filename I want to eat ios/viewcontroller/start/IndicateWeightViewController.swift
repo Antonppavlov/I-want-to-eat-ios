220980a9a50c9ddb6b09f45b5cfee4c26b49a7fd
antonpavlov <anton.it.pavlov@gmail.com>
2018-03-19 18:19:34 +0300

    добавил контроллеры к стартовым страницам

---
+++ I want to eat ios/viewcontroller/start/IndicateWeightViewController.swift
@@ -0,0 +1,35 @@
+//
+//  IndicateWeightViewController.swift
+//  I want to eat ios
+//
+//  Created by Anton Pavlov on 19.03.2018.
+//  Copyright © 2018 Anton Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class IndicateWeightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
