495d1567c56783defe02f3473dd2776bc518c1e0
antonpavlov <anton.it.pavlov@gmail.com>
2018-06-13 00:10:26 +0300

    страница настроек  + связь с хранилищем

--- I want to eat ios/viewcontroller/main/SettingsViewController.swift
+++ I want to eat ios/viewcontroller/main/SettingsViewController.swift
@@ -8,28 +8,52 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
-
+class SettingsViewController: KeyboardCloseViewController {
+    let storage = UserStorage()
+    
+    @IBOutlet weak var inputAge: UITextField!
+    @IBOutlet weak var inputHeight: UITextField!
+    @IBOutlet weak var inputWeight: UITextField!
+    @IBOutlet weak var inputActivity: UITextField!
+    @IBOutlet weak var inputTargetWeight: UITextField!
+    @IBOutlet weak var switchGender: UISegmentedControl!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        inputAge.text = String(storage.fetchAge()!)
+        inputHeight.text = String(storage.fetchHeight()!)
+        inputWeight.text = String(storage.fetchWeight()!)
+        inputActivity.text = String(storage.fetchActivity()!)
+        inputTargetWeight.text = String(storage.fetchTargetWeight()!)
+        
+        switchGender.selectedSegmentIndex = Int(storage.fetchGender()!)!
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func saveSettings(_ sender: Any) {
+        print(inputAge.text!)
+        print(inputHeight.text!)
+        print(inputWeight.text!)
+        print(inputActivity.text!)
+        print(inputTargetWeight.text!)
+        print(switchGender.selectedSegmentIndex)
+        
+        
+        
+        storage.saveAge(age: inputAge.text!)
+        storage.saveHeight(height: inputHeight.text!)
+        storage.saveWeight(weight: inputWeight.text!)
+        storage.saveActivity(activity: inputActivity.text!)
+        storage.saveTargetWeight(targetWeight: inputTargetWeight.text!)
+        
+        storage.saveGender(gender: String(switchGender.selectedSegmentIndex))
     }
-    */
-
+    
 }
+
+
+
+
